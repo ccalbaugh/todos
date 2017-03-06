87823b426131218cfe32195acc4635065e5ba786
Caden Albaugh <CadenAlbaugh@gmail.com>
2017-03-06 14:31:18 -0600

    Inline multiple actions for clarity

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,6 @@
 import { v4 } from 'node-uuid';
-import { getIsFetching } from '../reducers':
+import { getIsFetching } from '../reducers';
 import * as api from '../api';
-
-export requestTodos = (filter) => ({
-	type: 'REQUEST_TODOS',
-	filter,
-});
-
-const receiveTodos = (filter, response) => ({
-	type: 'RECIEVE_TODOS',
-	filter,
-	response,
-});
 
 export const fetchTodos = (filter) => (dispatch, getState) => {
 	// Now this func is conditionally calling the funcs that use network operations
@@ -21,11 +10,27 @@ export const fetchTodos = (filter) => (dispatch, getState) => {
 		return Promise.resolve();
 	}
 
-	dispatch(requestTodos(filter));
-
-	return api.fetchTodos(filter).then(response => {
-		dispatch(receiveTodos(filter, response));
+	dispatch({
+		type: 'FETCH_TODOS_REQUEST',
+		filter,
 	});
+
+	return api.fetchTodos(filter).then(
+		response => {
+			dispatch({
+				type: 'FETCH_TODOS_SUCCESS',
+				filter,
+				response,
+			});
+		},
+		error => {
+			dispatch({
+				type: 'FETCH_TODOS_FAILURE',
+				filter,
+				message: error.message || 'Something went wrong.'
+			});
+		}
+	);
 };
 
 
