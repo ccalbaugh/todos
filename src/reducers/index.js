8d9c4954c4016d7c739b981f196b3706db2b400c
Caden Albaugh <CadenAlbaugh@gmail.com>
2017-03-06 14:33:26 -0600

    Create selectors for fetching and errormessaging reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -20,5 +20,8 @@ export const getVisibleTodos = (state, filter) => { // but any export starting w
   return ids.map(id => fromById.getTodo(state.byId, id));
 };
 
-getIsFetching = (state, filter) =>
+export const getIsFetching = (state, filter) =>
   fromList.getIsFetching(state.listByFilter[filter]);
+
+export const getErrorMessage = (state, filter) =>
+  fromList.getErrorMessage(state.listByFilter[filter]);
