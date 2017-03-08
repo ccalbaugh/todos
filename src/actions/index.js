5036ba17763ec886b6d035b93ac526c15b93823b
Caden Albaugh <CadenAlbaugh@gmail.com>
2017-03-08 10:30:52 -0600

    Return normalized responses in action dispatchers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import { normalize } from 'normalizr';
+import * as schema from './schema';
 import { getIsFetching } from '../reducers';
 import * as api from '../api';
 
@@ -19,7 +21,7 @@ export const fetchTodos = (filter) => (dispatch, getState) => {
 			dispatch({
 				type: 'FETCH_TODOS_SUCCESS',
 				filter,
-				response,
+				response: normalize(response, schema.arrayOfTodos)
 			});
 		},
 		error => {
@@ -37,7 +39,7 @@ export const addTodo = (text) => (dispatch) =>
 	api.addTodo(text).then(response => {
 		dispatch({
 			type: 'ADD_TODO_SUCCESS',
-			response,
+			response: normalize(response, schema.todo)
 		});
 	});
 
